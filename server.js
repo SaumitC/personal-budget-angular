654ad689b303340449de79cb7ded37f27f32f4ab
Saumit Chinchkhandi <saumitc@Saumits-MacBook-Pro.local>
2023-09-24 17:34:32 -0400

    Added Cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
-app.use('/', express.static('public'));
+//app.use('/', express.static('public'));
+app.use(cors());
 
 /*const budget = {
     myBudget: [
